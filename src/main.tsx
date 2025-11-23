00ec4f80b61cb4365efc82505e56026d6dc3b548
chrisrimmer1 <32641184+chrisrimmer1@users.noreply.github.com>
2025-11-23 21:43:38 +0000

    Add dev-specific favicon with red border

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Use different favicon in development
+if (import.meta.env.DEV) {
+  const link = document.querySelector("link[rel*='icon']") as HTMLLinkElement;
+  if (link) {
+    link.href = '/sebmc-icon-dev.svg';
+  }
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
